abad006574a206d04a70e77a44aa8d6861c1cc04
Fernando A de la Fuente <60758926+delafern@users.noreply.github.com>
2020-02-12 13:02:32 -0800

    Update igniter_gui.py

--- igniter_gui.py
+++ igniter_gui.py
@@ -2,6 +2,8 @@ import serial.tools.list_ports
 from digi.xbee.devices import XBeeDevice
 from time import gmtime, strftime
 
+global text
+messages = []
 
 comport_list=[] #list of available ports for the user to choose from
 comport_list.append([comport.device for comport in serial.tools.list_ports.comports()])
@@ -10,22 +12,14 @@ device = XBeeDevice("COM8",9600)
 
 device.open()
 
-global text
-messages = []
-
 def data_receive_callback(xbee_message):
     text = "From %s %s >> %s" % (xbee_message.remote_device.get_64bit_addr(),strftime("%H:%M:%S"),xbee_message.data.decode())
     messages.append(text)
     print(text)
 
-
 device.add_data_received_callback(data_receive_callback)
 #device.send_data_broadcast('ping')
-#device.add_data_received_callback(data_receive_callback)
-
 
 device.send_data_broadcast('ping')
 
-
 print('hi')
-
